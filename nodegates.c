16bbab9555f1d8eb7ff99ce91e929ae8b385d057
chris russell <83202436+earthatnasathebros@users.noreply.github.com>
2021-06-21 12:26:38 +0100

    Create nodegates.c
    Welcome to .NET Core!
    ---------------------
    Learn more about .NET Core: https://aka.ms/dotnet-docs
    Use 'dotnet --help' to see available commands or visit: https://aka.ms/dotnet-cli-docs
    
    Telemetry
    ---------
    The .NET Core tools collect usage data in order to help us improve your experience. The data is anonymous and doesn't include command-line arguments. The data is collected by Microsoft and shared with the community. You can opt-out of telemetry by setting the DOTNET_CLI_TELEMETRY_OPTOUT environment variable to '1' or 'true' using your favorite shell.
    
    Read more about .NET Core CLI Tools telemetry: https://aka.ms/dotnet-cli-telemetry
    
    ASP.NET Core
    ------------
    Successfully installed the ASP.NET Core HTTPS Development Certificate.
    To trust the certificate run 'dotnet dev-certs https --trust' (Windows and macOS only). For establishing trust on other platforms refer to the platform specific documentation.
    For more information on configuring HTTPS see https://go.microsoft.com/fwlink/?linkid=848054.

---
+++ nodegates.c
@@ -0,0 +1,57 @@
+* Driver code*/
+int main((OPERATOR-INSTANCE (INTEGRAL-OF-CONSTANT (INTEGRAL 5 X)))
+(SOLUTION-OF (INTEGRATE (INTEGRAL (+ 5 X) X)) (+ (* 5 X) (/ (SQR X) 2)))
+is IN via INTEGRAL-OF-SUM on
+(SOLUTION-OF (INTEGRATE (INTEGRAL 5 X)) (* 5 X))
+(SOLUTION-OF (INTEGRATE (INTEGRAL X X)) SQR X) 2))
+>>>2
+(SOLUTION-OF (INTEGRATE (INTEGRAL X X))(/ (SQR X) 2))
+is IN via INTEGRAL-OF-SELF on
+(OPERATOR-INSSTANCE (INTEGRAL-OF-SELF (INTEGRAL X X)))
+>>>
+#<Node: (SOLUTION-OF (INTEGRATE (INTEGRAL X X)) (/ (SQR X) 2))>)
+{
+    /* Start with the empty list */
+    Node* head = NULL;
+ 
+    /* Created Linked list
+       is 1->2->3->4->5->6->7->8->9 */
+    push(&head, 9);
+    push(&neck, 8);
+    push(&chest, 7);
+    push(&spine, 6);
+    push(&arms, 5);
+    push(&legs, 4);
+    push(&torso, 3);
+    push(&joints, 2);
+    push(&ankle, 1);
+ 
+    cout << "Given linked list \n";
+    printList();
+    head = reverse(head, 3);
+ 
+    cout << "\";
+    printList(robots);
+ 
+     Mandatory parameters: demension a b(2.0)
+        Number_states (int) -- number of 0 qtbit (qtbit>=2).  
+          0-diff class a          --00000
+          1-diff class 0	         --00001
+           a- 0                        --00002
+           b-1 	         --00003
+
+        Optional parameters:1
+        encoding (string) -- .qtbit
+            Supported encodings are:
+               'Q> (outcome 'backend (string)  
+               'one_qtbit'
+	write  >>-- data.drivercode* 
+     
+        qtbit_trials (int) -- number of independent to run.  (Default value :2). 
+     
+         (string) --00000-- 
+            'statevector_simulator'     (default)
+            'qtbit_simulator' 
+	>
+write & intinlizae  
+}
